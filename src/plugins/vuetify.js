74b94271ea933f54774e33c66edec5f5c229f891
koarito <amiromarhonein@hotmail.fr>
2023-05-01 18:39:47 +0200

    init

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,17 @@
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+
+import { createVuetify } from "vuetify";
+
+export default createVuetify({
+  theme: {
+    themes: {
+      light: {
+        colors: {
+          primary: "#652d87",
+          secondary: "#E94F7C",
+        },
+      },
+    },
+  },
+});
---
+++ src/views/Home.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-container class="d-flex justify-center align-center h-screen">
+    <v-form v-model="valid">
+      <v-row>
+        <v-col>
+          <div class="text-h3">Welcome Back</div>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-text-field
+            v-model="email"
+            type="text"
+            label="Email adress"
+            color="primary"
+            style="width: 100%"
+          />
+
+          <v-text-field
+            v-model="password"
+            type="password"
+            label="Password"
+            color="primary"
+          />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <div class="d-flex justify-center">
+            <v-btn size="large" color="secondary">Login</v-btn>
+          </div>
+        </v-col>
+      </v-row>
+    </v-form>
+  </v-container>
+</template>
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const valid = ref(false);
+    const email = ref("");
+    const password = ref("");
+    return { valid, password, email };
+  },
+};
+</script>
+<style lang=""></style>
